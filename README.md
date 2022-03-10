91b78fdb21768b06916267b66a385192ad8a851f
Andrew Xia <axia@mit.edu>
2022-03-09 17:23:12 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles
+
+As I try to be a better developer myself, here is a list of dotfiles that can help improve my productivity
---
+++ configure.sh
@@ -0,0 +1,21 @@
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+if ! command -v fzf &> /dev/null
+then
+    echo "Installing fzf (fuzzy find)."
+    git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+    "$HOME/.fzf/install"
+else
+    echo "fzf already installed"
+fi
+
+if ! command -v aws &> /dev/null
+then
+    echo "AWS cli tool not found. Please install the AWS cli v2 using the instructions found here https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install"
+else
+    echo "aws already installed"
+fi
+
+# git
+echo "copying gitconfig"
+cp .gitconfig ~/
