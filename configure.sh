ae85b9c930fed9170717f5dd8bc71a9601c69c58
Andrew Xia <axia@mit.edu>
2024-05-07 15:48:18 -0700

    change configure

--- configure.sh
+++ configure.sh
@@ -9,19 +9,12 @@ else
     echo "fzf already installed"
 fi
 
-if ! command -v aws &> /dev/null
-then
-    echo "AWS cli tool not found. Please install the AWS cli v2 using the instructions found here https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install"
-else
-    echo "aws already installed"
-fi
-
 # git
 echo "copying gitconfig"
 cp .gitconfig ~/
 
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # other tools
-sudo snap install docker
-sudo chmod 666 /var/run/docker.sock
-sudo snap install tree
+brew install tree
